c0fa5a0d346d4cc980f8f9c2e04b7a9b8a03b365
qub333 <qub333@df67266a-131f-0410-a88b-938414686820>
2006-10-05 23:49:42 +0000

    Ok, things are almost kinda working now. M-Record linking is still shady, but everything else is at least fucntional...

--- LinkerLoader/src/Program.java
+++ LinkerLoader/src/Program.java
@@ -1,16 +1,18 @@
+import java.math.BigInteger;
+
 
 public class Program {
 	
 	public String programName;
-	public int length;
-	public int starting;
-	public Program(String arg0, int arg1, int arg2){
+	public BigInteger length;
+	public BigInteger starting;
+	public Program(String arg0, BigInteger arg1, BigInteger arg2){
 		programName = arg0;
 		length = arg1;
 		starting = arg2;
 	}
 	
 	public String toString(){
-		return String.format("%s , %s, %s ", programName, Integer.toHexString(length), Integer.toHexString(starting) );
+		return String.format("%s , %s, %s ", programName, LinkerLoader.bigIntToString(length), LinkerLoader.bigIntToString(starting) );
 	}
 }
