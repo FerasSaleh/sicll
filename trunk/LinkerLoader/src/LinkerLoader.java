394af8d1ed44b7405f72000ffd10ae1c5a1906ca
qub333 <qub333@df67266a-131f-0410-a88b-938414686820>
2006-10-05 19:23:55 +0000

    Some serious updates/overhauls to T-Record processing, and I'm starting on M-records.

--- trunk/LinkerLoader/src/LinkerLoader.java
+++ trunk/LinkerLoader/src/LinkerLoader.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -18,8 +19,11 @@ public class LinkerLoader {
 	private static int cs_addr = memory_start;
 	private static int execAddr = 0;
 	private static SymTab EsTab = new SymTab();
-	private static Program prog;
+	//private static Program prog;
 	public static Hashtable memory = new Hashtable();
+	
+	
+	
 	public static void main(String[] args) {
 		try {
 			BufferedReader br = new BufferedReader ( new FileReader ("ObjProgs.txt"));
@@ -59,7 +63,7 @@ public class LinkerLoader {
 			str = str.substring(1);
 			StringTokenizer st = new StringTokenizer(str);
 			
-			prog = new Program (st.nextToken().trim(), Integer.parseInt(st.nextToken(), 16), cs_addr);
+			Program prog = new Program (st.nextToken().trim(), Integer.parseInt(st.nextToken(), 16), cs_addr);
 			EsTab.insert(new Item (prog.programName, Integer.toHexString(cs_addr)));
 			cs_addr=cs_addr+prog.length;
 			System.out.println(prog + " " + Integer.toHexString(cs_addr));
@@ -73,10 +77,8 @@ public class LinkerLoader {
 					pos = pos+1;
 				Item i = new Item (str.substring(0, pos), 
 						Integer.toHexString((Integer.valueOf(str.substring(pos, pos+7).trim(),16)+cs_addr)));
-				System.out.println("Item: " + i);
-				EsTab.insert(i);
+				System.out.println("Item: " + EsTab.insert(i));
 				str = str.substring(pos+7);
-				//System.out.println(str);
 			}
 		} 
 	}
@@ -86,40 +88,74 @@ public class LinkerLoader {
 			cs_addr = Integer.valueOf((EsTab.find(str.substring(1,str.indexOf(' '))).getHexValue()), 16);
 			execAddr= cs_addr;
 		} else if (str.startsWith("T")){
-			str = str.substring(1);
+			//str = str.substring(1);
 			char[] chars = str.toCharArray();
-			
-		}
-			
-			
-			
-			
-			/*} else if ( str.startsWith("T")){
+			LinkedList ll = new LinkedList();
+			int ptr, lineStartAddr, lineSize;
+	        //obtain starting address
+	        String tmpStr="";
+	        for(ptr=1; ptr<7; ptr++) tmpStr += chars[ptr];
+	        lineStartAddr=Integer.parseInt(tmpStr.trim(), 16);
+	        lineStartAddr+=cs_addr;
+	        System.out.println (Integer.toHexString(lineStartAddr));
+	        tmpStr = "";
+	        for(; ptr<9; ptr++) tmpStr += chars[ptr];
+	        lineSize=Integer.parseInt(tmpStr.trim(), 16);
+	        //System.out.println(Integer.toHexString(lineSize));
+	        //System.out.println(lineSize);
+	        for(int i=0; i< lineSize; i++){
+	            int thisByte;
+	            tmpStr = "";
+	            tmpStr += chars[ptr++];
+	            tmpStr += chars[ptr++];
+	            thisByte = Integer.parseInt(tmpStr.trim(), 16);
+	            ll.add(thisByte);
+	        }
+	        Iterator itr = ll.iterator();
+	        String objCode="";
+	        while (itr.hasNext()){
+	        	objCode += Integer.toHexString(((Integer) itr.next()).intValue());
+	        }
+	        //System.out.println(objCode);
+	        if (!memory.containsKey(Integer.toHexString(lineStartAddr)))
+	        	memory.put(Integer.toHexString(lineStartAddr),objCode);
+	        else {
+	        	String temp = (String) memory.get(Integer.toHexString(lineStartAddr));
+	        	objCode = objCode + " " + temp;
+	        	memory.put(Integer.toHexString(lineStartAddr), objCode);
+	        }
+	        printMemoryMap();
+		} else if ( str.startsWith("M")){
 			str = str.substring(1);
-			String memLoc = str.substring(0, 6);
-			memLoc = addCSADDR(memLoc);
-			LinkedList<Byte[]> objCode = new LinkedList<Byte[]>();
-			str=str.substring(8);
-			System.out.print("Memory Location: " + memLoc + " ");
-			while (str.length() > 8){
-				Byte[] word = new Byte[4];
-				word[0]=(Byte.decode("#"+str.substring(0, 2)));
-				word[1]=(Byte.decode("#"+str.substring(2, 4)));
-				word[2]=(Byte.decode("#"+str.substring(4, 6)));
-				word[3]=(Byte.decode("#"+str.substring(6, 8)));
-				objCode.add(word);
-				System.out.print(((Byte[]) objCode.getLast())[0]);
-				System.out.print(((Byte[]) objCode.getLast())[1]);
-				System.out.print(((Byte[]) objCode.getLast())[2]);
-				System.out.print(((Byte[]) objCode.getLast())[3]+ " ");
-				str=str.substring(8);
+			String tempAddr ="";
+			int tempByte =0;
+			int index =0; 
+			for (index=0;index<6;index++){
+				tempAddr += str.charAt(index);
 			}
-			System.out.println();
-			memory.put(memLoc,objCode);
-			
-		}*/
+			tempByte = Integer.parseInt(tempAddr, 16);
+			//System.out.println(tempAddr);
+			//System.out.println(Integer.toHexString(tempByte));
+			char sign=' ';
+			if (str.contains("+"))
+				sign = '+';
+			else if (str.contains("-"))
+				sign = '-';
+			index=0;
+			index = str.indexOf(sign);
+			Item i = EsTab.find(str.substring(index+1));
+			//System.out.println(i);
+		}
+		
 	}
-	
+	private static void printMemoryMap(){
+		Enumeration enumer = memory.keys();
+		while (enumer.hasMoreElements()){
+			String str = (String) enumer.nextElement();
+			System.out.printf(" %s : %s \n", str, memory.get(str));
+		}
+	}
+
 	private static String addHexStrings(String s1, String s2){
 		return Integer.toHexString((Integer.valueOf(s1, 16)+Integer.valueOf(s2, 16)));
 	}
